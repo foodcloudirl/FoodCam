1ba17e340817436e8100a5c43f7d50e3d10b8158
James Clifford <engineer@jclifford.ie>
2017-06-21 17:40:10 +0100

    configurable high/low LED means on/off

--- settings-eg.py
+++ settings-eg.py
@@ -1,4 +1,7 @@
+import RPi.GPIO as GPIO
 slackUrl='https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXX'
 slackTestUrl='https://hooks.slack.com/services/XXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXX'
 location='the kitchen!'
 recipient='FoodCloud'
+on=GPIO.LOW
+off=GPIO.HIGH
